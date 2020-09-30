18aa8830a86154b718871b6f6ce4d7b1c62c41ae
xyzuan <i.xyzuan@gmail.com>
2020-09-30 21:08:08 +0000

    WeebCafe: migrate to androidx

--- src/com/weeb/settings/WeebSettings.java
+++ src/com/weeb/settings/WeebSettings.java
@@ -18,9 +18,9 @@
 package com.weeb.settings;
 
 import android.os.Bundle;
-import android.support.v7.preference.Preference;
-import android.support.v7.preference.PreferenceCategory;
-import android.support.v7.preference.PreferenceScreen;
+import androidx.preference.Preference;
+import androidx.preference.PreferenceCategory;
+import androidx.preference.PreferenceScreen;
 
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
